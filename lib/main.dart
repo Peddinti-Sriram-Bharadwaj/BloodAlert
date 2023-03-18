571b14e9d47a812ede7daae4d17d42b13f7432fb
SriramBharadwaj1 <sriram9217@gmail.com>
2023-03-18 21:50:57 +0530

    landing page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,58 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(BloodAlert());
+}
+
+class BloodAlert extends StatelessWidget {
+  const BloodAlert({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blood Alert',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Blood alert'),
+        ),
+        body: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Material(
+                elevation: 20,
+                child: InkWell(
+                  child: Ink(
+                    height: 100,
+                    width: 170,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Center(child: Text('This is box 1')),
+                  ),
+                  onTap: () {},
+                )),
+            Material(
+                elevation: 20,
+                child: InkWell(
+                  child: Ink(
+                      height: 100,
+                      width: 170,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Center(
+                          child: Text(
+                        'This is box 2',
+                      ))),
+                  onTap: () {},
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+}

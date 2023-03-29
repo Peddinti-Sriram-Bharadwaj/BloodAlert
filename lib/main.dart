0149e85ed8bea331a9b538255e4e7a14f236a2a5
SriramBharadwaj1 <sriram9217@gmail.com>
2023-03-29 15:08:25 +0530

    navivation and routing mechanism enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'alertpage.dart';
+import 'registerpage.dart';
 
 void main() {
-  runApp(BloodAlert());
+  runApp(const MaterialApp(
+    title: 'landing page',
+    home: Mainpage(),
+  ));
 }
 
-class BloodAlert extends StatelessWidget {
-  const BloodAlert({super.key});
+class Mainpage extends StatelessWidget {
+  const Mainpage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,42 +21,60 @@ class BloodAlert extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.red,
           title: const Text('Blood alert'),
         ),
         body: Center(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Material(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Material(
+                elevation: 30,
+                child: InkWell(
+                  child: Ink(
+                    height: 100,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text('Request for Blood'),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AlertPage()));
+                  },
+                ),
+              ),
+              Material(
                 elevation: 20,
                 child: InkWell(
                   child: Ink(
                     height: 100,
                     width: 170,
                     decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Center(child: Text('Request for Blood')),
-                  ),
-                  onTap: () {},
-                )),
-            Material(
-                elevation: 20,
-                child: InkWell(
-                  child: Ink(
-                      height: 100,
-                      width: 170,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10.0)),
-                      child: Center(
-                          child: Text(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text(
                         'become a blood donor',
-                      ))),
-                  onTap: () {},
-                ))
-          ],
-        )),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegisterPage()));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
